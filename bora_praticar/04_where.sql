e9980b30435d8faf595b17a837971845dcfe85fe
pabloestanislau <pablodata25@gmail.com>
2026-06-14 15:45:39 -0300

    adicionando 04_where

--- bora_praticar/04_where.sql
+++ bora_praticar/04_where.sql
@@ -1,3 +1,15 @@
 SELECT *
 FROM produtos 
+-- WHERE DescCategoriaProduto LIKE 'churn%'
 
+-- Churn_10pp
+-- Churn_2pp
+-- Churn_5pp
+
+-- WHERE DescProduto like 'churn'%
+
+-- WHERE DescNomeProduto = 'Churn_10pp'
+-- or DescNomeProduto = 'Churn_2pp'
+-- or DescNomeProduto = 'Churn_5pp'
+
+WHERE DescCategoriaProduto like 'churn%'
