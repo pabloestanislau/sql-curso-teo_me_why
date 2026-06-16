8be2a03ba9dcf172c9d1fc466eeaf7fab885886a
pabloestanislau <pablodata25@gmail.com>
2026-06-16 06:16:13 -0300

    adicionado 06_order_by

--- 06_order_by.sql
+++ 06_order_by.sql
@@ -1,4 +1,9 @@
+-- SELECT *
+-- FROM clientes
+-- ORDER BY QtdePontos
+-- LIMIT 10;
+
 SELECT *
-FROM clientes
-ORDER BY QtdePontos
-LIMIT 10;
+FROM  clientes 
+WHERE flTwitch = 1
+ORDER BY DtCriacao ASC, QtdePontos DESC;
